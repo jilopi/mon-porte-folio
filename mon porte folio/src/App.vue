656354e341c7591a19ace4d9f2238da3df9d65c8
Pierrick <jilopi.9@gmail.com>
2025-05-21 01:12:05 +0200

    Commit final : version prête à être livrée

--- mon porte folio/src/App.vue
+++ mon porte folio/src/App.vue
@@ -1,15 +1,48 @@
 <script setup>
-
+import Header from './components/header.vue'
+import Footer from './components/footer.vue'
 </script>
 
 
 
 <template>
-  
+  <Header />
+  <router-view />
+  <Footer />
 </template>
 
 
 
-<style scoped>
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+}
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: Arial, sans-serif;
+  background-color: #121212; /* ou blanc selon ton style */
+  color: white; /* ou noir selon le fond */
+  overflow-y: auto;
+}
 
+section h2 {
+  text-align: center;
+}
+
+section {
+  margin-bottom: 60px;
+  }
+  #presentation {
+  padding-top: 200px;
+}
+footer,header {
+  background-color: #0d1b2a;
+  }
+  section:hover h2 {
+  text-decoration: underline;
+}
+   
 </style>
---
+++ mon porte folio/src/components/CreationCard.vue
@@ -0,0 +1,54 @@
+<script setup>
+defineProps({
+  titre: String,
+  image: String,
+  description: String
+});
+</script>
+
+<template>
+  <div class="conteneur">
+  <div class="creation-card" @click="$emit('click')">
+    <h3>{{ titre }}</h3>
+    <div class="contenu">
+      <img :src="image" :alt="titre" />
+      <p>{{ description }}</p>
+    </div>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.creation-card {
+  border: 1px solid #ccc;
+  padding: 15px;
+  border-radius: 8px;
+  margin-bottom: 20px;
+}
+.creation-card:hover {
+  box-shadow: 8px 8px 20px rgba(0, 0, 0, 0.4);
+}
+
+
+.creation-card h3 {
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.contenu {
+  display: flex;
+  gap: 20px;
+  align-items: flex-start;
+}
+
+.contenu img {
+  width: 200px;
+  height: auto;
+  border-radius: 4px;
+}
+
+.contenu p {
+  flex: 1;
+  margin: 0;
+}
+</style>
